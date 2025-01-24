0b774acbd922567dac65a9d326013d141a1661bc
lucasvittal2 <lucasvittal@gmail.com>
2025-01-24 07:56:53 -0300

    built template for function and pipeline

---
+++ src/doc_ingestion_pipeline/beam/dofunctions/base_dofn.py
@@ -0,0 +1,65 @@
+import datetime
+import time
+import traceback
+from typing import Callable
+
+import apache_beam as beam
+import pytz  # type: ignore
+from apache_beam.metrics import Metrics
+
+from doc_ingestion_pipeline.utils.logging import LoggerHandler
+
+
+class BaseDoFn(beam.DoFn):
+    def _init_(self, logger_name: str):
+        super()._init_()
+        self.logger_name = logger_name
+        self.logger = LoggerHandler(
+            logger_name=self.logger_name, logging_type="console"
+        ).get_logger()
+        self.queue_size = 0
+        self.time_zone = pytz.timezone("America/Sao_Paulo")
+
+        namespace = self._class.module_
+        self.elements_processed = Metrics.counter(namespace, "elements_processed")
+        self.elements_failed = Metrics.counter(namespace, "elements_failed")
+        self.processing_time = Metrics.distribution(namespace, "processing_time_ms")
+        self.current_queue_size = Metrics.gauge(namespace, "current_queue_size")
+        self.unique_error_types = Metrics.string_set(namespace, "unique_error_types")
+
+    def deadletter_pdf_process(self, element, exception):
+        self.elements_failed.inc()
+        self.unique_error_types.add(type(exception)._name_)
+        self.logger.error(
+            f"Unexpected error on element ID: {element}: {str(exception)}"
+        )
+        return {
+            "pdf_id": element.get("pdf_id"),
+            "pdf_name": element.get("pdf_name"),
+            "number_pages": element.get("number_pages"),
+            "aspectsList": element.get(
+                "aspectsList", "This failed before the LLM call."
+            ),
+            "exceptionType": type(exception)._name_,
+            "exceptionMessage": str(exception),
+            "stackTrace": traceback.format_exc(),
+            "timestamp": datetime.datetime.now(tz=self.time_zone).isoformat() + "Z",
+        }
+
+    @classmethod
+    def gauge(cls, func: Callable) -> Callable:
+
+        def wrapper(self, *args, **kwargs):
+            start_time = time.time()
+            self.queue_size += 1
+            self.current_queue_size.set(self.queue_size)
+            try:
+                yield from func(self, *args, **kwargs)
+            finally:
+                self.queue_size = max(self.queue_size - 1, 0)
+                self.current_queue_size.set(self.queue_size)
+                end_time = time.time()
+                elapsed_time_ms = (end_time - start_time) * 1000
+                self.processing_time.update(elapsed_time_ms)
+
+        return wrapper
---
+++ src/doc_ingestion_pipeline/beam/pipelines/process_pdf.py
@@ -0,0 +1,101 @@
+from typing import List
+
+import apache_beam as beam
+from apache_beam.io import WriteToBigQuery
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.display import DisplayDataItem
+
+from doc_ingestion_pipeline.beam.dofunctions.base_dofn import BaseDoFn
+
+
+class ProcessPDFPipelineOptions(PipelineOptions):
+
+    @classmethod
+    def _add_argparse_args(cls, parser) -> None:
+        parser.add_argument(
+            "--bucket",
+            type=str,
+            required=True,
+            help="Bucket where pdf file path were uploaded",
+        )
+        parser.add_argument(
+            "--pub_sub_topic",
+            type=str,
+            required=True,
+            help="Topic to transmit the pdf upload event to pipeline",
+        )
+        parser.add_argument(
+            "--gcp_project", type=str, required=True, help="project where pipeline runs"
+        )
+        parser.add_argument(
+            "--output_file",
+            type=str,
+            required=True,
+            help="File path to output processed data to ingest on BigQuery",
+        )
+        parser.add_argument(
+            "--deadletter_file",
+            type=str,
+            required=True,
+            help="Deadletter BigQuery Table",
+        )
+
+
+class ProcessPdfPipeline(BaseDoFn, beam.Pipeline):
+    def __init__(self):
+        super().__init__(logger_name="[PROCESS-PDF-PIPELINE]")
+
+    def display_data(self) -> List[DisplayDataItem]:
+        return [
+            DisplayDataItem(
+                self.__class__.__name__, key="class_name", label="Class name"
+            ),
+            DisplayDataItem(
+                "Process Pdf Beam Pipeline", key="description", label="Description"
+            ),
+        ]
+
+    def run(self, argv=None):
+        pipeline_options = PipelineOptions(argv)
+        custom_options = pipeline_options.view_as(ProcessPDFPipelineOptions)
+
+        try:
+            self.logger.info("Starting Process Pdf Beam Pipeline...")
+            with beam.Pipeline(options=pipeline_options) as pipeline:
+                input_file = custom_options.input_file
+
+                transform_pdf_in_chunks = (
+                    pipeline
+                    | "ingest pdf"
+                    >> (lambda x: x)  # replace by correspondent do function
+                    | "split into chunks"
+                    >> (lambda x: x)  # replace by correspondent do function
+                )
+                generate_embeddings = (
+                    transform_pdf_in_chunks
+                    | "generate embeddings"
+                    >> (lambda x: x)  # replace by correspondent do function
+                )
+                write_on_alloydb = (
+                    generate_embeddings
+                    | "format table"
+                    >> (lambda x: x)  # replace by correspondent do function
+                    | "write on alloy db"
+                    >> (lambda x: x)  # replace by correspondent do function
+                )
+
+                deadletter_process_pdf = (
+                    write_on_alloydb
+                    | "Format Pdf processing Dead letter"
+                    >> beam.ParDo(lambda x: x)  # replace by correspondent do function
+                    | "Write ded summary on BigQuery"
+                    >> WriteToBigQuery()  # put parameters
+                )
+        except Exception as err:
+            self.logger.error(f"Error to retrieve pdf: \n\n{err}\n\n")
+            raise FileNotFoundError(f"pdf file not found: \n\n{err}\n\n")
+
+
+if __name__ == "__main__":
+    process_pdf_pipeline = ProcessPdfPipeline()
+    process_pdf_pipeline.run()
