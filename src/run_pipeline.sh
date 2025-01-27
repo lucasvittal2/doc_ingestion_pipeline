7f410cfd6fb9b3e2db89072d55ec06843c268623
lucasvittal2 <lucasvittal@gmail.com>
2025-01-27 07:57:07 -0300

    add shell to run pipeline locally

---
+++ src/run_pipeline.sh
@@ -0,0 +1,6 @@
+ python src/doc_ingestion_pipeline/beam/pipelines/process_pdf.py \
+    --bucket "test" \
+    --pub_sub_topic "test"\
+    --gcp_project "bot-especialist-dev"\
+    --output_file "test" \
+    --deadletter_file "test"
