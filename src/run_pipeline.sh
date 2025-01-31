0266952664b4ff16b843a5babd4c634c72722100
lucasvittal2 <lucasvittal@gmail.com>
2025-01-30 22:13:11 -0300

    implemented dataflow provisioning job

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
- python src/doc_ingestion_pipeline/beam/pipelines/process_pdf.py \
-    --bucket "test" \
-    --pub_sub_topic "test"\
-    --gcp_project "bot-especialist-dev"\
-    --output_file "test" \
-    --deadletter_file "test" \
-    --streaming
+python src/doc_ingestion_pipeline/beam/pipelines/process_pdf.py \
+    --runner=DataflowRunner \
+    --project="bot-especialist-dev" \
+    --bucket="doc-ingestion-pipeline-dev" \
+    --pub_sub_topic="projects/bot-especialist-dev/topics/ingestion-pipeline-dev" \
+    --template_location="gs://doc-ingestion-pipeline-dev/templates/doc_ingestion_template" \
+    --temp_location="gs://doc-ingestion-pipeline-dev/tmp" \
+    --region="us-central1" \
+    --deadletter_file="test"
