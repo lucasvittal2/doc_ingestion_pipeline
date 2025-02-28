f87ca43f136c7b749efd4dc3ae26a9057bc7dfd8
lucasvittal2 <lucasvittal@gmail.com>
2025-02-28 16:53:50 -0300

    implemente automatic provisioning of cloud function deployement used to trigger pdf-ingestion dataflow job and cloud schedule to trigger daily the cloud function

--- src/bash/provisioning.sh
+++ src/bash/provisioning.sh
@@ -119,7 +119,8 @@ create_pipeline_template() {
   PROJECT_NUMBER=$2
   REGION=$3
   REPOSITORY_NAME=$4
-  ENV=$5
+  CONTAINER_IMAGE=$5
+  ENV=$6
 
   if [[ -z "$PROJECT_ID" || -z "$PROJECT_NUMBER" || -z "$REGION" || -z "$REPOSITORY_NAME" || -z "$ENV" ]]; then
         echo "❌ Error: Missing parameters!"
@@ -135,18 +136,13 @@ create_pipeline_template() {
     --runner=DataflowRunner \
     --project="$PROJECT_ID" \
     --bucket="doc-ingestion-pipeline-${ENV}-${PROJECT_NUMBER}" \
-    --input_pub_sub_topic="projects/${PROJECT_ID}/topics/ingestion-pipeline-${ENV}" \
-    --output_pub_sub_topic="projects/${PROJECT_ID}/topics/doc-ingestion-output" \
-    --pub_sub_subscription="projects/${PROJECT_ID}/subscriptions/ingestion-pipeline-${ENV}-subscription" \
     --template_location="gs://doc-ingestion-pipeline-${ENV}-${PROJECT_NUMBER}/templates/doc_ingestion_template" \
     --temp_location="gs://doc-ingestion-pipeline-${ENV}-${PROJECT_NUMBER}/tmp" \
     --staging_location="gs://doc-ingestion-pipeline-${ENV}-${PROJECT_NUMBER}/staging" \
     --region="$REGION" \
-    --max_num_workers=2 \
-    --streaming \
+    --max_num_workers=1 \
     --experiments=shuffle_mode=service \
-    --sdk_container_image="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_NAME}/doc-ingestion-pipeline-${ENV}:v1" \
-    --flink_version=1.18 \
+    --sdk_container_image="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_NAME}/${CONTAINER_IMAGE}" \
     --save_main_session
 
   if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
@@ -201,6 +197,69 @@ create_project_gcp_resources() {
 
   cd "$PROJECT_DIR"
 }
+deploy_cloud_function() {
+    local FUNCTION_NAME="$1"
+    local REGION="$2"
+    local PROJECT_NUMBER="$3"
+    echo ""
+    echo "⚙️  Deploying cloud function '${FUNCTION_NAME}'..."
+
+    if gcloud functions describe "$FUNCTION_NAME" --region="$REGION" &> /dev/null; then
+        echo "⚠️ Warning: Cloud Function '$FUNCTION_NAME' is already deployed. Skipping deployment."
+        echo ""
+    else
+        echo ""
+        echo "🚀 Deploying Cloud Function '$FUNCTION_NAME'..."
+        gcloud functions deploy "$FUNCTION_NAME" \
+            --region="$REGION" \
+            --entry-point="trigger_ingestion_pipeline_http" \
+            --runtime="python310" \
+            --source="src/cloud_function/trigger_pdf_ingestion_dataflow_job" \
+            --service-account="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+            --trigger-http \
+            --allow-unauthenticated
+
+        echo "✅ Deployment completed for '$FUNCTION_NAME'."
+        echo ""
+    fi
+}
+
+create_cloud_scheduler_job() {
+    local JOB_NAME="$1"
+    local SCHEDULE="$2"
+    local TIMEZONE="$3"
+    local FUNCTION_NAME="$4"
+    local REGION="$5"
+
+    echo ""
+    echo "⚙️  Creating cloud scheduler job '${JOB_NAME}'..."
+    if gcloud functions describe "$FUNCTION_NAME" --region="$REGION" &> /dev/null; then
+        FUNCTION_URL=$(gcloud functions describe "trigger-pdf-ingestion-pipeline3" --region="us-central1" | grep url | sed "s/url: //g")
+        echo "✅ Cloud Function '$FUNCTION_NAME' found. URL: $FUNCTION_URL"
+        echo ""
+    else
+        echo "❌ Error: Cloud Function '$FUNCTION_NAME' not found in region '$REGION'. Exiting..."
+        echo ""
+        return 1
+    fi
+
+    # Check if the Cloud Scheduler job already exists
+    if gcloud scheduler jobs describe "$JOB_NAME" --location="$REGION" &> /dev/null; then
+        echo "⚠️ Warning: Cloud Scheduler job '$JOB_NAME' already exists. Skipping creation."
+        echo ""
+    else
+        echo "🚀 Creating Cloud Scheduler job '$JOB_NAME'..."
+        gcloud scheduler jobs create http "$JOB_NAME" \
+            --schedule="$SCHEDULE" \
+            --time-zone="$TIMEZONE" \
+            --uri="$FUNCTION_URL" \
+            --location="$REGION"
+
+        echo ""
+        echo "✅ Cloud Scheduler job '$JOB_NAME' created successfully."
+        echo ""
+    fi
+}
 
 # Collect Parameters
 echo "running script with the following parameters:"
@@ -214,7 +273,7 @@ while [[ $# -gt 0 ]]; do
     ;;
   --pipeline-bucket)
     PIPELINE_BUCKET="$2"
-    echo "ENV=$ENV"
+    echo "PIPELINE_BUCKET=$PIPELINE_BUCKET"
     shift 2
     ;;
   --registry-repo-name)
@@ -235,12 +294,32 @@ while [[ $# -gt 0 ]]; do
 
   --project-number)
     PROJECT_NUMBER="$2"
-    echo "PROJECT_ID=$PROJECT_ID"
+    echo "PROJECT_NUMBER=$PROJECT_ID"
     shift 2
     ;;
   --region)
     REGION="$2"
-    echo "PROJECT_ID=$PROJECT_ID"
+    echo "REGION=$REGION"
+    shift 2
+    ;;
+  --cloud-function-name)
+    CLOUD_FUNCTION_NAME="$2"
+    echo "CLOUD_FUNCTION_NAME=$CLOUD_FUNCTION_NAME"
+    shift 2
+    ;;
+  --cron-job-name)
+    CRON_JOB_NAME="$2"
+    echo "CRON_JOB_NAME=$CRON_JOB_NAME"
+    shift 2
+    ;;
+  --cron-shedule)
+    CRON_SCHEDULE="$2"
+    echo "CRON_SCHEDULE=$CRON_SCHEDULE"
+    shift 2
+    ;;
+  --cron-timezone)
+    CRON_TIMEZONE="$2"
+    echo "CRON_TIMEZONE=$CRON_TIMEZONE"
     shift 2
     ;;
   *)
@@ -251,10 +330,12 @@ esac
 done
 
 # Main Execution
-REGISTRY_URL="us-central1-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_NAME}/${CONTAINER_IMAGE}"
+REGISTRY_URL="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_NAME}/${CONTAINER_IMAGE}"
 create_gcs_pipeline_bucket "$PIPELINE_BUCKET" "$PROJECT_ID"
 build_container "$REGISTRY_URL"
 create_artifact_repo "$REPOSITORY_NAME" "$PROJECT_ID"
 push_container_gcp "$REGISTRY_URL"
-create_pipeline_template "$PROJECT_ID" "$PROJECT_NUMBER" "$REGION" "$REPOSITORY_NAME" "$ENV"
+create_pipeline_template "$PROJECT_ID" "$PROJECT_NUMBER" "$REGION" "$REPOSITORY_NAME" "$CONTAINER_IMAGE" "$ENV"
 create_project_gcp_resources "$REGION"
+deploy_cloud_function "$CLOUD_FUNCTION_NAME" "$REGION" "$PROJECT_NUMBER"
+create_cloud_scheduler_job "$CRON_JOB_NAME" "$CRON_SCHEDULE" "$CRON_TIMEZONE" "$CLOUD_FUNCTION_NAME" "$REGION"
